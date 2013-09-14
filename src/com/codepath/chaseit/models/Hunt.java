b5009676e69c30ed5942de418457fcc8e87c6858
8indaas <8indaas@gmail.com>
2013-09-14 12:50:17 -0700

    Initial Commit

---
+++ src/com/codepath/chaseit/models/Hunt.java
@@ -0,0 +1,12 @@
+package com.codepath.chaseit.models;
+
+import java.io.Serializable;
+
+public class Hunt implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7325191126592989417L;
+
+}
---
+++ src/com/codepath/chaseit/models/Location.java
@@ -0,0 +1,7 @@
+package com.codepath.chaseit.models;
+
+import java.io.Serializable;
+
+public class Location implements Serializable {
+
+}
