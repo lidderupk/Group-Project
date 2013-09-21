b990c29d94f528ce5be74bc9f9d60a940d59a40e
Bianca <gando.bia@gmail.com>
2013-09-21 14:48:14 -0400

    tabbed hunt feeds

--- ChaseIt/src/com/chaseit/activities/HomeScreenActivity.java
+++ ChaseIt/src/com/chaseit/activities/HomeScreenActivity.java
@@ -1,66 +1,66 @@
 package com.chaseit.activities;
 
+
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.ActionBar.TabListener;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
+import android.view.View;
 
 import com.chaseit.R;
 import com.chaseit.fragments.MyHuntsFragment;
 import com.chaseit.fragments.NewsFeedFragment;
 import com.chaseit.fragments.RecentHuntsFragment;
-//import android.support.v4.app.FragmentManager;
 
 public class HomeScreenActivity extends FragmentActivity implements TabListener {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) { 
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setupNavigationTabs();
 		setContentView(R.layout.activity_news_feed);
-//
-//		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-//		ft.replace(R.id.flNewsFeedFragment, new NewsFeedFragment());
-//		ft.commit();
-	}
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.news_feed, menu);
-		return true;
-	}
-	
-    private void setupNavigationTabs() {
-    	ActionBar actionBar = getActionBar();
-    	actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-    	actionBar.setDisplayShowTitleEnabled(true);
-    	Tab tabHome = actionBar.newTab().setText("Newsfeed")
-    			.setTag("HomeTimelineFragment")
-    			.setIcon(R.drawable.ic_logo)
-    			.setTabListener(this);
-    	
-    	Tab tabRecent = actionBar.newTab().setText("Recent Hunts")
-    			.setTag("RecentFragment")
-    			.setIcon(R.drawable.ic_logo)
-    			.setTabListener(this);
-    	Tab tabMine = actionBar.newTab().setText("My Hunts")
-    			.setTag("MyHuntsFragment")
-    			.setIcon(R.drawable.ic_logo)
-    			.setTabListener(this);
-    	
-    	actionBar.addTab(tabHome);
-    	actionBar.addTab(tabMine);
-    	actionBar.addTab(tabRecent);
-    	actionBar.selectTab(tabHome);
+		setupNavigationTabs();
 	}
 
-	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-		// TODO Auto-generated method stub
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	private void setupNavigationTabs() {
+		ActionBar actionBar = getActionBar();
+
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		
+		Tab tabHome = actionBar.newTab().setText("All Hunts")
+				.setTag("HomeTimelineFragment").setTabListener(this);
+
+		Tab tabRecent = actionBar.newTab().setText("Recent")
+				.setTag("RecentFragment").setTabListener(this);
+		Tab tabMine = actionBar.newTab().setText("Mine")
+				.setTag("MyHuntsFragment").setTabListener(this);
+
+		actionBar.addTab(tabHome);
+		actionBar.addTab(tabMine);
+		actionBar.addTab(tabRecent);
+		actionBar.selectTab(tabHome);
+
+		actionBar.setDisplayShowHomeEnabled(false);
+		//displaying custom ActionBar w Menu icon to the left
+		View mActionBarView = getLayoutInflater().inflate(R.layout.my_action_bar, null);
+		actionBar.setCustomView(mActionBarView);
+		actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+
+
+	}
+
+	public void onProfileSelected(View v){
+		Intent i = new Intent(HomeScreenActivity.this, UserDetailsActivity.class);
+		startActivity(i);
 	}
 
 	@Override
@@ -72,16 +72,17 @@ public class HomeScreenActivity extends FragmentActivity implements TabListener 
 	@Override
 	public void onTabSelected(Tab tab, android.app.FragmentTransaction ft) {
 		FragmentManager manager = getSupportFragmentManager();
-		android.support.v4.app.FragmentTransaction fts = manager.beginTransaction();
-		if(tab.getTag() == "HomeTimelineFragment"){
+		android.support.v4.app.FragmentTransaction fts = manager
+				.beginTransaction();
+		if (tab.getTag() == "HomeTimelineFragment") {
 			fts.replace(R.id.flNewsFeedFragment, new NewsFeedFragment());
-			
+
 		} else if (tab.getTag() == "RecentHuntsFragment") {
 			fts.replace(R.id.flNewsFeedFragment, new RecentHuntsFragment());
 		} else {
 			fts.replace(R.id.flNewsFeedFragment, new MyHuntsFragment());
 		}
-		
+
 		fts.commit();
 	}
 
@@ -90,5 +91,4 @@ public class HomeScreenActivity extends FragmentActivity implements TabListener 
 		// TODO Auto-generated method stub
 		
 	}
-
 }
